5bceb883eaf41edc480e009ddfa494ebfffb4b4d
mshahbaz <mshahbaz@cs.princeton.edu>
2016-05-15 01:45:33 -0400

    added a script for building OVS/DPDK

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# TODO: this should probably be a makefile
+# TODO: install target
+(
+cd deps/dpdk
+if [[ ! -e Makefile ]]
+then
+	echo "ERROR: dpdk submodule not initialized"
+	echo "Please run git submodule update --init"
+	exit 1
+fi
+patch -p1 -N < ../../setup-scripts/dpdk.patch
+make -j 8 install T=x86_64-native-linuxapp-gcc
+
+cd ../
+source ./setup-env-vars.sh
+
+cd ../
+./boot.sh
+./configure --with-dpdk=$DPDK_BUILD CFLAGS="-g -O2 -Wno-cast-align"
+#            \
+#            p4inputfile=./include/p4/examples/l2_switch/l2_switch.p4 \
+#            p4outputdir=./include/p4/src
+make clean
+make -j 8
+)
