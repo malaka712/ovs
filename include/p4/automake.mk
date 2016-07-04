a1ee565404e761b88ed7f42190048dbc6279dd43
Muhammad Shahbaz <mshahbaz@cs.princeton.edu>
2016-07-04 16:07:54 -0500

    adding simple_router files in automake.mk.

--- include/p4/automake.mk
+++ include/p4/automake.mk
@@ -1,4 +1,8 @@
 EXTRA_DIST += \
 	include/p4/README.md \
 	include/p4/examples/l2_switch/l2_switch.p4 \
-	include/p4/examples/l2_switch/l2_switch.sh
+	include/p4/examples/l2_switch/l2_switch.sh \
+	include/p4/examples/l2_switch/nativel2_switch.sh \
+	include/p4/examples/simple_router/simple_router.p4 \
+	include/p4/examples/simple_router/simple_router.sh \
+	include/p4/examples/simple_router/native_simple_router.sh
